f76da37a7f400c10419268b1adf0995cca16a442
Rashedin-063 <rashedinislam.06@gmail.com>
2025-12-31 17:00:58 +0600

    home and about page ui updated

--- client/app/about.tsx
+++ client/app/about.tsx
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 const AboutPage = () => {
   return (
-    <View>
-      <Text>about</Text>
-    </View>
-  )
+    <SafeAreaProvider>
+      <SafeAreaView style={aboutStyles.aboutContainer}>
+        <View>
+          <Text style={aboutStyles.titleText}>About Page</Text>
+          <Link href="/" style={aboutStyles.linkText}>Home</Link>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
 export default AboutPage
+
+
+const aboutStyles = StyleSheet.create({
+  aboutContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    padding: 20
+  },
+  titleText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  linkText: {
+    color: '#007AFF',
+    textDecorationLine: 'underline',
+    marginTop: 10,
+    fontSize: 24,
+    textAlign: 'center'
+  }
+})
