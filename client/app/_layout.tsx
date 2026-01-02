d980c6421ee9fd3c9e4d947c55fb1e8dbb07d050
Rashedin-063 <rashedinislam.06@gmail.com>
2026-01-02 20:55:15 +0600

    error solved

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,13 +1,9 @@
-import SafeScreen from "@/components/SafeScreen";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name='(tabs)' />
       </Stack>
-      ;
-    </SafeScreen>
-  );
+  )
 }
