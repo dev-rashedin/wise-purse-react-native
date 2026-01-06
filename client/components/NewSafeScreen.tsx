7b2231ddb3c37606fdd934449d4390995f6d46f2
dev-rashedin <rashedinislam.06@gmail.com>
2026-01-06 21:11:56 +0600

    new component added

---
+++ client/components/NewSafeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NewSafeScreen = () => {
+  return (
+    <View>
+      <Text>NewSafeScreen 123</Text>
+    </View>
+  )
+}
+
+export default NewSafeScreen
