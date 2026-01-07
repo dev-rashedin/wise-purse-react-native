fdf447f2427b4c45b2579d7a7061cbb5e1f8f9d8
dev-rashedin <rashedinislam.06@gmail.com>
2026-01-07 11:49:38 +0600

    home page ui updated

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function Index() {
             alignItems: 'center',
           }}
         >
-          <Text>Edit app/index.tsx to edit this screen.</Text>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>This is the home page</Text>
           <Image
             source={{
               uri: 'https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
