43a9ac9718215ccf053238daa9450aefe0216022
dev-rashedin <rashedinislam.06@gmail.com>
2026-01-16 14:15:32 +0600

    optional cron.ts file added in the server

--- server/src/app/config/index.ts
+++ server/src/app/config/index.ts
@@ -3,12 +3,14 @@ interface Config {
   port: number;
   databaseUrl?: string;
   jwtSecret?: string;
+  api_url?: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   databaseUrl: process.env.DATABASE_URL,
   jwtSecret: process.env.JWT_SECRET,
+  api_url: process.env.API_URL
 };
 
 export default config;
